85237c52d0afd22eb3c1af1e7d639f21fa5bfde9
Gareth Rushgrove <gareth@morethanseven.net>
2008-11-22 13:00:01 +0000

    seperated out django assertions and added README

--- common.py
+++ common.py
@@ -41,25 +41,7 @@ class Common(TestCase):
         cursor = connection.cursor()
         cursor.execute(*sql)
         return cursor
-      
-    def login_as_admin(self):
-        "Create, then login as, an admin user"
-        user = User.objects.create_user('admin', 'admin@example.com', 'password')
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        if not self.client.login(username='admin', password='password'):
-            raise Exception("Login failed")
-  
-    # Some assertions need to know which template tag libraries to load
-    # so we provide a list of templatetag libraries        
-    template_tag_libraries = []
-
-    def render(self, template, **kwargs):
-        "Return the rendering of a given template including loading of template tags"        
-        template = "".join(["{%% load %s %%}" % lib for lib in self.template_tag_libraries]) + template
-        return Template(template).render(Context(kwargs)).strip()
-  
+          
     # Custom assertions
     
     def assert_equal(self, *args, **kwargs):
@@ -78,26 +60,10 @@ class Common(TestCase):
         "Assert that one value (the hasystack) does not contain another value (the needle)"
         return self.assert_(needle not in haystack, "Content should not contain `%s' but does:\n%s" % (needle, haystack))
 
-    def assert_response_contains(self, fragment, response):
-        "Assert that a response object contains a given string"
-        self.assert_(fragment in response.content, "Response should contain `%s' but doesn't:\n%s" % (fragment, response.content))
-
-    def assert_response_doesnt_contain(self, fragment, response):
-        "Assert that a response object does not contain a given string"
-        self.assert_(fragment not in response.content, "Response should not contain `%s' but does:\n%s" % (fragment, response.content))
-
     def assert_regex_contains(self, pattern, string, flags=None):
         "Assert that the given regular expression matches the string"
         flags = flags or 0
         self.assertTrue(re.search(pattern, string, flags) != None)
-
-    def assert_render_matches(self, template, match_regexp, vars={}):
-        "Assert than the output from rendering a given template with a given context matches a given regex"
-        r = re.compile(match_regexp)
-        actual = Template(template).render(Context(vars))
-        self.assert_(r.match(actual), "Expected: %s\nGot: %s" % (
-            match_regexp, actual
-        ))
         
     def assert_count(self, expected, model):
         "Assert that their are the expected number of instances of a given model"
@@ -114,10 +80,6 @@ class Common(TestCase):
     def assert_is_instance(self, model, obj):
         "Assert than a given object is an instance of a model"
         self.assert_(isinstance(obj, model), "%s should be instance of %s" % (obj, model))
-
-    def assert_code(self, response, code):
-        "Assert that a given response returns a given HTTP status code"
-        self.assertEqual(code, response.status_code, "HTTP Response status code %d expected, but got %d" % (code, response.status_code))
         
     def assert_raises(self, *args, **kwargs):
         "Assert than a given function and arguments raises a given exception"
@@ -141,26 +103,3 @@ class Common(TestCase):
     def assert_file_exists(self, file_path):
         "Assert a given file exists"
         self.assertTrue(os.path.exists(file_path), "%s does not exist!" % file_path)
-    
-    def assert_render(self, expected, template, **kwargs):
-        "Asserts than a given template and context render a given fragment"
-        self.assert_equal(expected, self.render(template, **kwargs))
-
-    def assert_render_matches(self, match_regexp, template, vars={}):
-        r = re.compile(match_regexp)
-        actual = Template(template).render(Context(vars))
-        self.assert_(r.match(actual), "Expected: %s\nGot: %s" % (
-            match_regexp, actual
-        ))
-
-    def assert_doesnt_render(self, expected, template, **kwargs):
-        "Asserts than a given template and context don't render a given fragment"
-        self.assert_not_equal(expected, self.render(template, **kwargs))
-
-    def assert_render_contains(self, expected, template, **kwargs):
-        "Asserts than a given template and context rendering contains a given fragment"
-        self.assert_contains(expected, self.render(template, **kwargs))
-
-    def assert_render_doesnt_contain(self, expected, template, **kwargs):
-        "Asserts than a given template and context rendering does not contain a given fragment"
-        self.assert_doesnt_contain(expected, self.render(template, **kwargs))
