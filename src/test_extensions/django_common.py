a790a9e3e5548a71a1f4e8861c042ee0078ec4b2
Philip Plumlee <philipplumlee@Cuker-Designs-iMac.local>
2010-01-25 15:28:54 -0800

    upgrade assert_regex_contains

--- src/test_extensions/django_common.py
+++ src/test_extensions/django_common.py
@@ -10,18 +10,18 @@ from django.template import Template, Context
 class DjangoCommon(Common):
     """
     This class contains a number of custom assertions which
-    extend the default Django assertions. Use this as the super 
+    extend the default Django assertions. Use this as the super
     class for you tests rather than django.test.TestCase
     """
-    
+
     # a list of fixtures for loading data before each test
     fixtures = []
-        
+
     def setUp(self):
         """
         setUp is run before each test in the class. Use it for
         initilisation and creating mock objects to test
-        """        
+        """
         pass
 
     def tearDown(self):
@@ -29,10 +29,10 @@ class DjangoCommon(Common):
         tearDown is run after each test in the class. Use it for
         cleaning up data created during each test
         """
-        pass 
+        pass
 
     # A few useful helpers methods
-          
+
     def login_as_admin(self):
         "Create, then login as, an admin user"
         # Only create the user if they don't exist already ;)
@@ -43,10 +43,10 @@ class DjangoCommon(Common):
             user.is_staff = True
             user.is_superuser = True
             user.save()
-        
+
         if not self.client.login(username='admin', password='password'):
             raise Exception("Login failed")
-  
+
     # Some assertions need to know which template tag libraries to load
     # so we provide a list of templatetag libraries
     template_tag_libraries = []
@@ -55,9 +55,9 @@ class DjangoCommon(Common):
         "Return the rendering of a given template including loading of template tags"
         template = "".join(["{%% load %s %%}" % lib for lib in self.template_tag_libraries]) + template
         return Template(template).render(Context(kwargs)).strip()
-  
+
     # Custom assertions
-    
+
     def assert_response_contains(self, fragment, response):
         "Assert that a response object contains a given string"
         self.assert_(fragment in response.content, "Response should contain `%s' but doesn't:\n%s" % (fragment, response.content))
@@ -69,7 +69,16 @@ class DjangoCommon(Common):
     def assert_regex_contains(self, pattern, string, flags=None):
         "Assert that the given regular expression matches the string"
         flags = flags or 0
-        self.assertTrue(re.search(pattern, string, flags) != None)
+        disposition = re.search(pattern, string, flags)
+        self.assertTrue(disposition != None, pattern + ' not found in ' + string)
+
+    def deny_regex_contains(self, pattern, slug):
+        r = re.compile(pattern)
+
+        self.assertEqual( None,
+                          r.search(smart_str(slug)),
+                          pattern + ' should not match ' + smart_str(slug) )
+
 
     def assert_render_matches(self, template, match_regexp, vars={}):
         "Assert than the output from rendering a given template with a given context matches a given regex"
@@ -78,11 +87,11 @@ class DjangoCommon(Common):
         self.assert_(r.match(actual), "Expected: %s\nGot: %s" % (
             match_regexp, actual
         ))
-        
+
     def assert_code(self, response, code):
         "Assert that a given response returns a given HTTP status code"
         self.assertEqual(code, response.status_code, "HTTP Response status code %d expected, but got %d" % (code, response.status_code))
-            
+
     def assert_render(self, expected, template, **kwargs):
         "Asserts than a given template and context render a given fragment"
         self.assert_equal(expected, self.render(template, **kwargs))
