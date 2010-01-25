f426cf603dc4e2711e61c234fb1ea530257870ac
Philip Plumlee <philipplumlee@Cuker-Designs-iMac.local>
2010-01-25 15:47:37 -0800

    add sternly worded comment

--- src/test_extensions/common.py
+++ src/test_extensions/common.py
@@ -16,18 +16,18 @@ from django.contrib.auth.models import User
 class Common(TestCase):
     """
     This class contains a number of custom assertions which
-    extend the default Django assertions. Use this as the super 
+    extend the default Django assertions. Use this as the super
     class for you tests rather than django.test.TestCase
     """
-    
+
     # a list of fixtures for loading data before each test
     fixtures = []
-        
+
     def setUp(self):
         """
         setUp is run before each test in the class. Use it for
         initilisation and creating mock objects to test
-        """        
+        """
         pass
 
     def tearDown(self):
@@ -35,18 +35,18 @@ class Common(TestCase):
         tearDown is run after each test in the class. Use it for
         cleaning up data created during each test
         """
-        pass 
+        pass
 
     # A few useful helpers methods
-    
+
     def execute_sql(*sql):
         "execute a SQL query and return the cursor"
         cursor = connection.cursor()
         cursor.execute(*sql)
         return cursor
-          
+
     # Custom assertions
-    
+
     def assert_equal(self, *args, **kwargs):
         "Assert that two values are equal"
         return self.assertEqual(*args, **kwargs)
@@ -54,7 +54,7 @@ class Common(TestCase):
     def assert_not_equal(self, *args, **kwargs):
         "Assert that two values are not equal"
         return not self.assertNotEqual(*args, **kwargs)
-    
+
     def assert_contains(self, needle, haystack):
         "Assert that one value (the hasystack) contains another value (the needle)"
         return self.assert_(needle in haystack, "Content should contain `%s' but doesn't:\n%s" % (needle, haystack))
@@ -63,16 +63,16 @@ class Common(TestCase):
         "Assert that one value (the hasystack) does not contain another value (the needle)"
         return self.assert_(needle not in haystack, "Content should not contain `%s' but does:\n%s" % (needle, haystack))
 
-    def assert_regex_contains(self, pattern, string, flags=None):
+    def assert_regex_contains(self, pattern, string, flags=None):  #  TODO  get dry with django_common's clones of this!!!
         "Assert that the given regular expression matches the string"
         flags = flags or 0
         self.assertTrue(re.search(pattern, string, flags) != None)
-        
+
     def assert_count(self, expected, model):
         "Assert that their are the expected number of instances of a given model"
         actual = model.objects.count()
         self.assert_equal(expected, actual, "%s should have %d objects, had %d" % (model.__name__, expected, actual))
-        
+
     def assert_counts(self, expected_counts, models):
         "Assert than a list of numbers is equal to the number of instances of a list of models"
         if len(expected_counts) != len(models):
@@ -83,7 +83,7 @@ class Common(TestCase):
     def assert_is_instance(self, model, obj):
         "Assert than a given object is an instance of a model"
         self.assert_(isinstance(obj, model), "%s should be instance of %s" % (obj, model))
-        
+
     def assert_raises(self, *args, **kwargs):
         "Assert than a given function and arguments raises a given exception"
         return self.assertRaises(*args, **kwargs)
@@ -101,7 +101,7 @@ class Common(TestCase):
             self.assertTrue(key in item)
         except AssertionError:
             print 'no %s in %s' % (key, item)
-            raise AssertionError 
+            raise AssertionError
 
     def assert_file_exists(self, file_path):
         "Assert a given file exists"
