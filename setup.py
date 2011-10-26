e77d2cb16820fd4d5030ba52bbfe148a96ae8066
Gareth Rushgrove <gareth@morethanseven.net>
2011-10-26 20:20:49 +0100

    bump version due to django 1.3 fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-test-extensions",
-    version = "0.13",
+    version = "0.14",
     author = "Gareth Rushgrove",
     author_email = "gareth@morethanseven.net",
     url = "http://github.com/garethr/django-test-extensions/",
